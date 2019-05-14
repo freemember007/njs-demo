241367b723f4ace8ab274fcb0a264866f9a9d46d
freemember007 <freemember007@gmail.com>
2019-05-14 21:08:39 +0800

    同上

--- js/hi.js
+++ js/hi.js
@@ -1,45 +1,63 @@
 function foo(r) {
-    r.log("hello from foo() handler");
-    return "foo";
+  r.log("hello from foo() handler")
+  return "foo"
 }
 
 function summary(r) {
-    var a, s, h;
+  var a, s, h
 
-    s = "JS summary\n\n";
+  s = "JS summary\n\n"
 
-    s += "Method: " + r.method + "\n";
-    s += "HTTP version: " + r.httpVersion + "\n";
-    s += "Host: " + r.headersIn.host + "\n";
-    s += "Remote Address: " + r.remoteAddress + "\n";
-    s += "URI: " + r.uri + "\n";
+  s += "Method: " + r.method + "\n"
+  s += "HTTP version: " + r.httpVersion + "\n"
+  s += "Host: " + r.headersIn.host + "\n"
+  s += "Remote Address: " + r.remoteAddress + "\n"
+  s += "URI: " + r.uri + "\n"
 
-    s += "Headers:\n";
-    for (h in r.headersIn) {
-        s += "  header '" + h + "' is '" + r.headersIn[h] + "'\n";
-    }
+  s += "Headers:\n"
+  for (h in r.headersIn) {
+    s += "  header '" + h + "' is '" + r.headersIn[h] + "'\n"
+  }
 
-    s += "Args:\n";
-    for (a in r.args) {
-        s += "  arg '" + a + "' is '" + r.args[a] + "'\n";
-    }
-
-    return s;
+  s += "Args:\n"
+  for (a in r.args) {
+      s += "  arg '" + a + "' is '" + r.args[a] + "'\n"
+  }
+  s += r.requestBody
+  r.error('1111111111111111111')
+  return s
 }
 
 function baz(r) {
-    r.status = 200;
-    r.headersOut.foo = 1234;
-    r.headersOut['Content-Type'] = "text/plain; charset=utf-8";
-    r.headersOut['Content-Length'] = 15;
-    r.sendHeader();
-    r.send("nginx");
-    r.send("java");
-    r.send("script");
-
-    r.finish();
+  r.status = 200
+  r.headersOut.foo = 1234
+  r.headersOut['Content-Type'] = "text/plain charset=utf-8"
+  r.headersOut['Content-Length'] = 16
+  r.sendHeader()
+  r.send("nginx ")
+  r.send("javascript")
+  r.finish()
 }
 
 function hello(r) {
-    r.return(200, "Hello world!23");
+  var s
+  s = 'hello world '
+  s += r.requestBody
+  s += ' '
+  s += typeof(r.requestBody)
+  s += ' '
+  // r.args不是真正的对象
+  // s += typeof(r.args) 
+  // s += JSON.stringify(r.args)
+  // s += Object.values(r.args) 
+  // s += Object.entries(r.args) 
+  s += JSON.stringify({ m: 1, n: true })
+  for (var a in r.args) {
+      s += "  arg '" + a + "' is '" + r.args[a] + "'\n"
+  }
+  for (var i in r.variables) {
+      s += "  var '" + i + "' is '" + r.variables[i] + "'\n"
+  }
+  r.error(r.args.c)
+  r.return(200, s)
 }
